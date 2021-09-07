d2442539772dc5275e9f6726935e37e75aff963c
Timon Klinkert <timon.klinkert2005@gmail.com>
2021-09-07 20:18:39 +0200

    Creating `createConfig` and `ifGuildDocExists` Methods in `Database.java`

--- src/main/java/com/denux/slashy/services/Database.java
+++ src/main/java/com/denux/slashy/services/Database.java
@@ -2,6 +2,7 @@ package com.denux.slashy.services;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+import com.denux.slashy.Bot;
 import com.denux.slashy.properties.ConfigString;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -14,6 +15,7 @@ import com.mongodb.client.MongoDatabase;
 import net.dv8tion.jda.api.entities.Guild;
 import org.bson.Document;
 import org.slf4j.LoggerFactory;
+
 import static com.mongodb.client.model.Filters.eq;
 
 public class Database {
@@ -34,11 +36,14 @@ public class Database {
     }
 
     public JsonElement getConfig(Guild guild, String path) {
-        //TODO return null
-        try {
+
+        if (ifGuildDocExists(guild)) {
+
+            //Connection to the cluster
             MongoDatabase database = mongoClient.getDatabase("other");
             MongoCollection<Document> collection = database.getCollection("config");
 
+            //Getting the doc by searching after the guild id
             String doc = collection.find(eq("guild_id", guild.getId())).first().toJson();
             String[] split = path.split("\\.");
 
@@ -46,12 +51,53 @@ public class Database {
             for (int i = 0; i < split.length - 1; i++) root = root.get(split[i]).getAsJsonObject();
 
             return root.get(split[split.length - 1]);
-        } catch (NullPointerException e) {
-            JsonElement j = new JsonPrimitive("0");
-            return j;
+
         }
+    else {
+        //Creating a Config if the guild doesn't have one
+        createConfig(guild);
 
+        return new JsonPrimitive("0");
+        }
+    }
 
+    //checks if the guild has a config or not
+    private boolean ifGuildDocExists(Guild guild) {
+
+        MongoDatabase database = mongoClient.getDatabase("other");
+        MongoCollection<Document> collection = database.getCollection("config");
+
+        Document doc = collection.find(eq("guild_id", guild.getId())).first();
+
+        if (doc == null) return false;
+
+        else return true;
+
+    }
+
+    //Creating the Config for a guild who doesn't have one
+    public void createConfig(Guild guild) {
+
+        //Connection to the cluster
+        MongoDatabase database = mongoClient.getDatabase("other");
+        MongoCollection<Document> collection = database.getCollection("config");
+
+        //Creating the Database structure
+        if (!ifGuildDocExists(guild)) {
+            Document doc = new Document()
+                    .append("guild_id", guild.getId())
+                    .append("logChannel", "0")
+                    .append("muteRole", "0")
+                    .append("serverLock", false)
+                    .append("starboardChannel", "0")
+                    .append("warnLimit", "0");
+
+            collection.insertOne(doc);
+
+            //Returning a feedback via the Logger Plugin
+            Bot.logger.info("Creating a Config with the guild id: "+guild.getId());
+        }
+        else Bot.logger.error("Guild already has a config.");
     }
 }
 
