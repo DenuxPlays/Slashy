17f1f3f6770adf50b1680dc2d75ef92b48476274
-Denux <timon.klinkert2005@gmail.com>
2021-09-21 18:38:02 +0200

    Updated `TempBanListener` and `TempMuteListener`

--- src/main/java/com/denux/slashy/listener/TempBanListener.java
+++ src/main/java/com/denux/slashy/listener/TempBanListener.java
@@ -40,49 +40,54 @@ public class TempBanListener {
                     .append("unbanTime", new BasicDBObject("$lte", Instant.now()));
 
             try {
-                Document doc = tempBan.find(criteria).first();
+                var it = tempBan.find(criteria).iterator();
+                while (it.hasNext()) {
+                    Document doc = it.next();
+                    try {
+                        String guildID = doc.getString("guildID");
+                        String memberID = doc.getString("memberID");
+                        String moderatorID = doc.getString("moderatorID");
+                        String banTime = doc.getString("banTime");
+                        String reason = doc.getString("reason");
 
-                String guildID = doc.getString("guildID");
-                String memberID = doc.getString("memberID");
-                String moderatorID = doc.getString("moderatorID");
-                String banTime = doc.getString("banTime");
-                String reason = doc.getString("reason");
+                        Guild guild = jda.getGuildById(guildID);
+                        User user = jda.retrieveUserById(memberID).complete();
+                        User moderator = jda.retrieveUserById(moderatorID).complete();
 
-                Guild guild = jda.getGuildById(guildID);
-                User user = jda.retrieveUserById(memberID).complete();
-                User moderator = jda.retrieveUserById(moderatorID).complete();
+                        try {
+                            guild.unban(user).queue();
+                        } catch (Exception exception) {
+                            logger.warn("User is already unbanned.");
+                        } finally {
+                            tempBan.deleteOne(doc);
+                        }
 
-                try {
-                    guild.unban(user).queue();
-                } catch (Exception exception) {
-                    logger.warn("User is already unbanned.");
-                } finally {
-                    tempBan.deleteOne(doc);
+                        String logChannelID = new Database().getConfig(guild, "logChannel").getAsString();
+                        if (!logChannelID.equals("0")) {
+
+                            TextChannel logChannel = guild.getTextChannelById(logChannelID);
+
+                            var embed = new EmbedBuilder()
+                                    .setAuthor(user.getAsTag() + "| Unban", null, user.getEffectiveAvatarUrl())
+                                    .setColor(Constants.YELLOW)
+                                    .setTimestamp(Instant.now())
+                                    .addField("Banned by", "```" + moderator.getAsTag() + "```", false)
+                                    .addField("Banned on", "```" + banTime + " | UTC ```", true)
+                                    .addField("Reason", "```" + reason + "```", false)
+                                    .setFooter(jda.getSelfUser().getAsTag() + Constants.FOOTER_MESSAGE, jda.getSelfUser().getEffectiveAvatarUrl())
+                                    .build();
+
+                            logChannel.sendMessageEmbeds(embed).queue();
+                        }
+                    } catch (Exception exception) {
+                        logger.error("Error caused. Deleting Document.");
+                        tempBan.deleteOne(doc);
+                    }
                 }
-
-                String logChannelID = new Database().getConfig(guild, "logChannel").getAsString();
-                if (!logChannelID.equals("0")) {
-
-                    TextChannel logChannel = guild.getTextChannelById(logChannelID);
-
-                    var embed = new EmbedBuilder()
-                            .setAuthor(user.getAsTag() + "| Unban", null, user.getEffectiveAvatarUrl())
-                            .setColor(Constants.YELLOW)
-                            .setTimestamp(Instant.now())
-                            .addField("Banned by", "```" + moderator.getAsTag() + "```", false)
-                            .addField("Banned on", "```" + banTime + " | UTC ```", true)
-                            .addField("Reason", "```" + reason + "```", false)
-                            .setFooter(jda.getSelfUser().getAsTag() + Constants.FOOTER_MESSAGE, jda.getSelfUser().getEffectiveAvatarUrl())
-                            .build();
-
-                    logChannel.sendMessageEmbeds(embed).queue();
-                }
-
             } catch (NullPointerException exception) {
                 logger.warn(exception.getClass().getSimpleName());
             } catch (Exception exception) {
-                Document doc = tempBan.find(criteria).first();
-                tempBan.deleteOne(doc);
+                logger.error(exception.getClass().getSimpleName());
             }
         }, 0, 30, TimeUnit.SECONDS);
     }
--- src/main/java/com/denux/slashy/listener/TempMuteListener.java
+++ src/main/java/com/denux/slashy/listener/TempMuteListener.java
@@ -41,41 +41,47 @@ public class TempMuteListener {
                     .append("unmuteTime", new BasicDBObject("$lte", Instant.now()));
 
             try {
-                Document doc = tempMute.find(criteria).first();
+                var it = tempMute.find(criteria).iterator();
+                while (it.hasNext()) {
+                    Document doc = tempMute.find(criteria).first();
+                    try {
+                        String guildID = doc.getString("guildID");
+                        String memberID = doc.getString("memberID");
+                        String moderatorID = doc.getString("moderatorID");
+                        String muteTime = doc.getString("muteTime");
+                        String reason = doc.getString("reason");
 
-                String guildID = doc.getString("guildID");
-                String memberID = doc.getString("memberID");
-                String moderatorID = doc.getString("moderatorID");
-                String muteTime = doc.getString("muteTime");
-                String reason = doc.getString("reason");
+                        Guild guild = jda.getGuildById(guildID);
+                        Member member = guild.getMemberById(memberID);
+                        Member moderator = guild.getMemberById(moderatorID);
 
-                Guild guild = jda.getGuildById(guildID);
-                Member member = guild.getMemberById(memberID);
-                Member moderator = guild.getMemberById(moderatorID);
+                        String muteRoleID = new Database().getConfig(guild, "muteRole").getAsString();
+                        Role muteRole = guild.getRoleById(muteRoleID);
 
-                String muteRoleID = new Database().getConfig(guild, "muteRole").getAsString();
-                Role muteRole = guild.getRoleById(muteRoleID);
+                        guild.removeRoleFromMember(member, muteRole).complete();
 
-                guild.removeRoleFromMember(member, muteRole).complete();
+                        tempMute.deleteOne(doc);
+                        String logChannelID = new Database().getConfig(guild, "logChannel").getAsString();
+                        if (!logChannelID.equals("0")) {
 
-                tempMute.deleteOne(doc);
+                            TextChannel logChannel = guild.getTextChannelById(logChannelID);
 
-                String logChannelID = new Database().getConfig(guild, "logChannel").getAsString();
-                if (!logChannelID.equals("0")) {
+                            var embed = new EmbedBuilder()
+                                    .setAuthor(member.getUser().getAsTag() + "| Unmute", null, member.getUser().getEffectiveAvatarUrl())
+                                    .setColor(Constants.YELLOW)
+                                    .setTimestamp(Instant.now())
+                                    .addField("Muted by", "```" + moderator.getUser().getAsTag() + "```", false)
+                                    .addField("Muted on", "```" + muteTime + " | UTC ```", true)
+                                    .addField("Reason", "```" + reason + "```", false)
+                                    .setFooter(jda.getSelfUser().getAsTag() + Constants.FOOTER_MESSAGE, jda.getSelfUser().getEffectiveAvatarUrl())
+                                    .build();
 
-                    TextChannel logChannel = guild.getTextChannelById(logChannelID);
-
-                    var embed = new EmbedBuilder()
-                            .setAuthor(member.getUser().getAsTag() + "| Unmute", null, member.getUser().getEffectiveAvatarUrl())
-                            .setColor(Constants.YELLOW)
-                            .setTimestamp(Instant.now())
-                            .addField("Muted by", "```" + moderator.getUser().getAsTag() + "```", false)
-                            .addField("Muted on", "```" + muteTime + " | UTC ```", true)
-                            .addField("Reason", "```" + reason + "```", false)
-                            .setFooter(jda.getSelfUser().getAsTag() + Constants.FOOTER_MESSAGE, jda.getSelfUser().getEffectiveAvatarUrl())
-                            .build();
-
-                    logChannel.sendMessageEmbeds(embed).queue();
+                            logChannel.sendMessageEmbeds(embed).queue();
+                        }
+                    } catch (Exception exception) {
+                        logger.error("Error caused. Deleting Document.");
+                        tempMute.deleteOne(doc);
+                    }
                 }
             } catch (NullPointerException exception) {
                 logger.warn(exception.getClass().getSimpleName());
